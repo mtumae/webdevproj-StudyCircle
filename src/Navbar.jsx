e17d42acad8967f52a475bb8ef314225f6216d3d
mtume <mtume2016@gmail.com>
2025-07-10 09:51:30 +0300

    change color on active link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,20 +3,29 @@ import './Navbar.css'
 import { Sunset } from 'lucide-react'
 import { Moon } from 'lucide-react'
 import { Sunrise } from 'lucide-react'
-
+import { NavLink } from 'react-router-dom'
 
 export default function NavBar(){
     const date = new Date()
-
-
-
     return(
         <div class="header">
             <h1>StudyCirlce</h1>
             <div class="inner-header">
-                <a href='/'>Home</a>
-                <a href='/about'>About</a>
-                <a href='/contact'>Contact</a>
+                <NavLink   to='/' >
+                    {({isActive})=>(
+                        <span className={isActive?"active-link":''}>Home</span>
+                    )}
+                </NavLink>
+                <NavLink  to='/about'>
+                    {({isActive})=>(
+                        <span className={isActive?"active-link":''}>About</span>
+                    )}
+                </NavLink>
+                <NavLink to='/contact'>
+                    {({isActive})=>(
+                        <span className={isActive?"active-link":''}>Contact</span>
+                    )}
+                </NavLink>
                 {date.getHours()>=12&&date.getHours()<17?
                 <p className='flex-box'>Good afternoon <Sun /></p>
                 :date.getHours()>=17&&date.getHours()<19?
